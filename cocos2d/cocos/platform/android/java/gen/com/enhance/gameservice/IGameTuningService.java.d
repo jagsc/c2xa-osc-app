c62c292568399a557432e3852ba9d4b04b3d2f74
NewNotMoon <new.not.moon@gmail.com>
2015-08-31 00:19:22 +0900

    漏れファイルコミット

---
+++ cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java.d
@@ -0,0 +1,3 @@
+Y:\jagsc\c2xa-osc-app\cocos2d\cocos\platform\android\java\gen\com\enhance\gameservice\IGameTuningService.java: \
+  Y:\jagsc\c2xa-osc-app\cocos2d\cocos\platform\android\java\src\com\enhance\gameservice\IGameTuningService.aidl 
+
---
+++ proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+Y:\jagsc\c2xa-osc-app\proj.android\gen\org\jagst\osccocos2dxapp\R.java \
+ : Y:\jagsc\c2xa-osc-app\proj.android\res\drawable-hdpi\icon.png \
+Y:\jagsc\c2xa-osc-app\proj.android\res\drawable-ldpi\icon.png \
+Y:\jagsc\c2xa-osc-app\proj.android\res\drawable-mdpi\icon.png \
+Y:\jagsc\c2xa-osc-app\proj.android\res\values\strings.xml \
+Y:\jagsc\c2xa-osc-app\proj.android\bin\AndroidManifest.xml \
