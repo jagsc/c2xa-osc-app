31155e77baad2c6abd89546ee206246f5e3bd5cf
NewNotMoon <new.not.moon@gmail.com>
2015-10-22 17:35:47 +0900

    asset削除

--- proj.android/gen/R.java.d
+++ proj.android/gen/R.java.d
@@ -1,6 +1,6 @@
-C:\Users\mu-\Documents\Github\c2xa-osc-app\proj.android\gen\org\jagst\osccocos2dxapp\R.java \
- : C:\Users\mu-\Documents\Github\c2xa-osc-app\proj.android\res\drawable-hdpi\icon.png \
-C:\Users\mu-\Documents\Github\c2xa-osc-app\proj.android\res\drawable-ldpi\icon.png \
-C:\Users\mu-\Documents\Github\c2xa-osc-app\proj.android\res\drawable-mdpi\icon.png \
-C:\Users\mu-\Documents\Github\c2xa-osc-app\proj.android\res\values\strings.xml \
-C:\Users\mu-\Documents\Github\c2xa-osc-app\proj.android\bin\AndroidManifest.xml \
+Y:\jagsc\c2xa-osc-app\proj.android\gen\org\jagst\osccocos2dxapp\R.java \
+ : Y:\jagsc\c2xa-osc-app\proj.android\res\drawable-hdpi\icon.png \
+Y:\jagsc\c2xa-osc-app\proj.android\res\drawable-ldpi\icon.png \
+Y:\jagsc\c2xa-osc-app\proj.android\res\drawable-mdpi\icon.png \
+Y:\jagsc\c2xa-osc-app\proj.android\res\values\strings.xml \
+Y:\jagsc\c2xa-osc-app\proj.android\bin\AndroidManifest.xml \
